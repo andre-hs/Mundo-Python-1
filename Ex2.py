dadfcf502ad8e4cc8e8b892f42beb73b15b43c74
andre-hs <andrehs.corretor@gmail.com>
2025-03-16 13:55:54 -0300

    Renomear arquivos e adicionar novos scripts Python

---
+++ Ex2.py
@@ -0,0 +1,5 @@
+# Respondendo ao Usuário
+
+print ('Digite seu nome abaixo:')
+nome = input ('Nome:')
+print ('Seja Bem vindo,',nome)
---
+++ Ex3.py
@@ -0,0 +1,6 @@
+# Somando dois números
+
+n1 = int(input("Escolha um número: "))
+n2 = int(input("Escolha outro número: "))
+soma = n1 + n2
+print("A soma dos números é: ", soma)
